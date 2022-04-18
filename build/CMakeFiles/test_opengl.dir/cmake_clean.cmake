32d5efbc26f08dace3e603f9c711f08e4b6004a1
sunweie <intermin@163.com>
2022-04-18 20:59:28 +0800

    first commit

---
+++ build/CMakeFiles/test_opengl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_opengl.dir/test_gl.cpp.o"
+  "test_opengl"
+  "test_opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
